d269bed165c584a5e16a21d67f6256d4835e0823
code2life <joey.yang@zoom.us>
2019-09-16 15:53:24 +0800

    fix type definition

--- index.d.ts
+++ index.d.ts
@@ -7,8 +7,12 @@ export interface NtlmType2Msg {
   targetInfo?: object;
 }
 
-export default interface NtlmClient {
+export interface NtlmClient {
   createType1Message: (workstation: string, target: string) => string;
   decodeType2Message: (str: string) => NtlmType2Msg;
   createType3Message: (type2Message: NtlmType2Msg, username: string, password: string, workstation?: string, target?: string) => string;
-};
+}
+
+declare var ntlmClient: NtlmClient;
+
+export default ntlmClient;
